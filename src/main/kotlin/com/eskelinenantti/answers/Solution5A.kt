4b98ddc1e52d40835ca7267c94fbedc3c6fbc728
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2023-12-05 19:01:08 +0200

    Optimized memory usage

--- src/main/kotlin/com/eskelinenantti/answers/Solution5A.kt
+++ src/main/kotlin/com/eskelinenantti/answers/Solution5A.kt
@@ -2,29 +2,29 @@ package com.eskelinenantti.answers
 
 import com.eskelinenantti.resources.inputForPuzzle
 
-
 fun main() {
     val rows = inputForPuzzle(5)
     val seeds = rows.first().removePrefix("seeds: ").split(" ").map(String::toLong)
 
-    val maps = mutableListOf<MutableMap<Long, Long>>()
+    // Just parse the string input to sane format
+    val maps = mutableListOf<MutableList<Triple<Long, Long, Long>>>()
     rows.filter { it.isNotBlank() }.drop(1).forEach { row ->
         if (row.first().isDigit()) {
-            val map = row.split(" ").map(String::toLong)
-            val (target, source, range) = map
-            repeat(range.toInt()) { index ->
-                maps.last()[source + index] = target + index
-            }
+            val (target, source, range) = row.split(" ").map(String::toLong)
+            maps.last().add(Triple(target, source, range))
         } else {
-            maps.add(mutableMapOf())
+            maps.add(mutableListOf())
         }
     }
 
     val result = seeds.minOf { seed ->
         maps.fold(seed) { acc, map ->
-            map.getOrElse(acc) { acc }
+            map.firstOrNull {
+                (it.second..<it.second + it.third).contains(acc)
+            }?.run {
+                first + (acc - second)
+            } ?: acc
         }
     }
-
     println(result)
 }
